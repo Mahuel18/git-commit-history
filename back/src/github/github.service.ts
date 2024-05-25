0f5ad36e2f699556e560c970946d8a5a18393e52
Mahuel18 <ezohan18@gmail.com>
2024-05-25 07:31:12 -0400

    feat: create GitHub service
    Generate a service to handle API requests:
    - Created GitHub service using NestJS CLI command
    - Implemented logic to fetch commits from a GitHub repository

---
+++ back/src/github/github.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+
+@Injectable()
+export class GithubService {
+    constructor(private readonly httpService: HttpService) {}
+
+  async getCommits(owner: string, repo: string): Promise<AxiosResponse<any>> {
+    const url = `https://api.github.com/repos/${owner}/${repo}/commits`;
+    return this.httpService.get(url).toPromise();
+  }
+}
