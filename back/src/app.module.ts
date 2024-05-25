b5b7de9b20cd47330d0d71f600347158da9bf70d
Mahuel18 <ezohan18@gmail.com>
2024-05-25 08:08:45 -0400

    fix: imports corrected
    Corrected some imports in the app.module file

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,12 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GithubService } from './github/github.service';
 import { GithubController } from './github/github.controller';
+import {HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [],
-  controllers: [AppController, GithubController],
-  providers: [AppService, GithubService],
+  imports: [HttpModule.register({
+    timeout: 5000,
+    maxRedirects: 5,
+  }),],
+  controllers: [GithubController],
+  providers: [GithubService],
 })
 export class AppModule {}
